aac257d1a0a74d94b6ea5f014e0fa14f5e78b4f9
Prathamesh Devhare <prathamesh.kali@gmail.com>
2023-07-17 11:13:28 +0530

    started

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page padding>
+    <input v-model="Msg" />
+    <button @click="clear">clear</button>
+    <h5>{{ Msg }}</h5>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      Msg: "I am a message",
+    };
+  },
+  methods: {
+    clear() {
+      this.Msg = "";
+    },
+  },
+};
+</script>
